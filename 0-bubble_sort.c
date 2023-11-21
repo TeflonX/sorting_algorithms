27da97b87ef7fbb2e826ceaf29369ed96d968e7d
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-11-21 11:07:18 +0000

    task 0: fixed array == NULL edge case

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,19 +13,20 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 
 	int swap;
-
-	for (i = 0; i < size - 1; i++)
+	if (array)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - 1; j++)
 			{
-				swap = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = swap;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = swap;
+					print_array(array, size);
+				}
 			}
 		}
 	}
-
 }
