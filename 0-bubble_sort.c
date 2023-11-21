8479c66c1711f33c3ea315c51adffe2cd5a818e6
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-11-21 10:57:00 +0000

    task 0: bubble sort implemented

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	int swap;
+
+	for(i = 0; i < size - 1; i++)
+	{
+		for(j = 0; j < size - 1; j++)
+		{
+			if(array[j] > array[j+1])
+			{
+				swap = array[j];
+				array[j] = array[j+1];
+				array[j+1] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+
+}
