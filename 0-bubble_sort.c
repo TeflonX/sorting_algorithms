470d72dab415cb6f3835986326f20401cbdd4b76
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-11-21 11:44:55 +0000

    task 0: optimized code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,22 +12,26 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	int swap;
+	int swap, flag;
 
 	if (array)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
-			for (j = 0; j < size - 1; j++)
+			flag = 0;
+			for (j = 0; j < size - i - 1; j++)
 			{
 				if (array[j] > array[j + 1])
 				{
 					swap = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = swap;
+					flag = 1;
 					print_array(array, size);
 				}
 			}
+			if (flag == 0)
+				break;
 		}
 	}
 }
