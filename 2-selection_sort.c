f723dc926dd435102e4a56841b82a7523ba96330
TeflonX <wisdom.ikegwuru@yahoo.com>
2023-11-22 03:26:02 +0000

    task 2: fixed the null pointer argument condition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,22 +13,23 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min;
 
 	int swap;
-
-	for (i = 0; i < size - 1; i++)
+	if (array)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] < array[min])
-				min = j;
-		}
-		if (i != min)
-		{
-			swap = array[i];
-			array[i] = array[min];
-			array[min] = swap;
-			print_array(array, size);
+			min = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[j] < array[min])
+					min = j;
+			}
+			if (i != min)
+			{
+				swap = array[i];
+				array[i] = array[min];
+				array[min] = swap;
+				print_array(array, size);
+			}
 		}
 	}
-
 }
